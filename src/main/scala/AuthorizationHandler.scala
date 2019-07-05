d604e2fb5b4e21b5241aa5550f5138dd2dc0964b
lwronski <lukas97w@onet.pl>
2019-07-05 09:03:21 +0200

    Validate token

--- src/main/scala/AuthorizationHandler.scala
+++ src/main/scala/AuthorizationHandler.scala
@@ -1,8 +1,12 @@
-
 import akka.http.scaladsl.model.headers.OAuth2BearerToken
-import akka.http.scaladsl.server.Directives.{extractCredentials, reject}
+import akka.http.scaladsl.server.Directives.{extractCredentials, onComplete, provide, reject}
 import akka.http.scaladsl.server.{AuthorizationFailedRejection, Directive1}
+import org.keycloak.adapters.{KeycloakDeployment, KeycloakDeploymentBuilder}
 import org.keycloak.representations.AccessToken
+import org.keycloak.adapters.rotation.AdapterTokenVerifier
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+
 
 trait AuthorizationHandler{
 
@@ -10,13 +14,29 @@ trait AuthorizationHandler{
   def authorize: Directive1[AccessToken] =
     extractCredentials.flatMap {
       case Some(OAuth2BearerToken(token)) =>
-        println(s"token $token is valid")
+          onComplete( Future {
+            verifyToken(token)
+          }
+          ).flatMap {
+            provide(AdapterTokenVerifier.verifyToken(token, keycloakDeployment))
+            _.map(accessToken => provide(accessToken)).recover {
+              case e =>
+                reject(AuthorizationFailedRejection)
+                  .toDirective[Tuple1[AccessToken]]
+            }.get
+          }
         reject(AuthorizationFailedRejection)
       case _ =>
         println("no token present in request")
         reject(AuthorizationFailedRejection)
+
     }
 
+  def verifyToken(token: String): AccessToken = {
+    AdapterTokenVerifier.verifyToken(token, keycloakDeployment)
+  }
 
+  val keycloakDeployment: KeycloakDeployment =
+    KeycloakDeploymentBuilder.build(getClass.getResourceAsStream("/assets/keycloak.json"))
 
 }
